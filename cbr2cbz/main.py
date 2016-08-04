fdb1e15a0ed554bb86c43b7545b618429036470f
Derrick <dh@dadi.co>
2016-08-04 15:28:47 -0500

    Add support for converting entire directories

--- cbr2cbz/main.py
+++ cbr2cbz/main.py
@@ -2,10 +2,30 @@
 
 import argparse
 import os.path
+import os
 import zipfile
 import sys
+import glob
 
 import rarfile
+
+def get_output_name(path):
+    """Given some path of form /my/dir/myfile.ext, return myfile.cbz
+
+    Arguments:
+        path - the input path which points to a specific file
+
+    Return:
+        A filename ending in .cbz
+    """
+
+    if not path:
+        return "comic.cbz"
+    name = path.split("/")[-1]
+    if name.find(".cbr"):
+        return name.replace(".cbr", ".cbz")
+    else:
+        return name + ".cbz"
 
 def convert_file(cbr, output):
     """Given a string pointing to a cbr file, output a cbz file to output
@@ -38,9 +58,14 @@ parser.add_argument("target", nargs="?", default=".", help="Output path, optiona
 
 args = vars(parser.parse_args());
 if os.path.isdir(args["target"]):
-    args["target"] = os.path.join(args["target"], args["source"].split("/")[-1].replace(".cbr", ".cbz"))
+    args["target"] = os.path.join(os.getcwd(), args["target"], args["source"].split("/")[-1].replace(".cbr", ".cbz"))
 if os.path.isdir(args["source"]):
-    quit()
+    comics = glob.glob(os.path.join(os.getcwd(), args["source"], "*.cbr"))
+    for comic in comics:
+        comic_path = os.path.join(args["source"], comic)
+        comic_name = get_output_name(comic)
+        comic_output = os.path.join(args["target"], comic_name)
+        convert_file(comic_path, comic_output)
 else:
     convert_file(args["source"], args["target"])
 quit()
